aa5a47452e37e73107a5b51a0407fdeb4eabdfa5
Victor <vasconcellosdevictor@gmail.com>
2025-02-25 17:29:34 -0300

    25-02

--- data-canary/monster/mammals/brown_rat.lua
+++ data-canary/monster/mammals/brown_rat.lua
@@ -1,10 +1,10 @@
-local mType = Game.createMonsterType("Rat")
+local mType = Game.createMonsterType("Brown Rat")
 local monster = {}
 
-monster.description = "a rat"
-monster.experience = 5
+monster.description = "a brown rat"
+monster.experience = 10
 monster.outfit = {
-	lookType = 21,
+	lookType = 56,
 	lookHead = 0,
 	lookBody = 0,
 	lookLegs = 0,
@@ -13,11 +13,7 @@ monster.outfit = {
 	lookMount = 0,
 }
 
-monster.events = {
-	"RationalRequestRatDeath",
-}
-
-monster.raceId = 21
+monster.raceId = 56
 monster.Bestiary = {
 	class = "Mammal",
 	race = BESTY_RACE_MAMMAL,
@@ -27,15 +23,17 @@ monster.Bestiary = {
 	CharmsPoints = 5,
 	Stars = 1,
 	Occurrence = 0,
-	Locations = "Rookgaard and Mainland, in most sewers and caves near towns. They can be found almost everywhere in Tibia.",
+	Locations = "Almost everywhere in tibia, they seem to have a nest-like place in Greenshore, \z
+		a semi-large spawn at the entrance to the Port Hope troll cave and in cave near Ankrahmun ship. \z
+		Also appears in Rat Plague in Thais and Rat Plague in Rookgaard.",
 }
 
-monster.health = 20
-monster.maxHealth = 20
+monster.health = 30
+monster.maxHealth = 30
 monster.race = "blood"
-monster.corpse = 5964
-monster.speed = 67
-monster.manaCost = 200
+monster.corpse = 4255
+monster.speed = 75
+monster.manaCost = 250
 
 monster.changeTarget = {
 	interval = 4000,
@@ -58,7 +56,7 @@ monster.flags = {
 	canPushCreatures = false,
 	staticAttackChance = 90,
 	targetDistance = 1,
-	runHealth = 5,
+	runHealth = 3,
 	healthHidden = false,
 	isBlockable = false,
 	canWalkOnEnergy = false,
@@ -75,34 +73,37 @@ monster.voices = {
 	interval = 5000,
 	chance = 10,
 	{ text = "Meep!", yell = false },
+	{ text = "Meeeeep!", yell = false },
 }
 
 monster.loot = {
-	{ name = "gold coin", chance = 100000, maxCount = 4 },
-	{ id = 3607, chance = 39410 }, -- cheese
+	{ name = "gold coin", chance = 85000, maxCount = 2 },
+	{ name = "cookie", chance = 750 },
+	{ id = 3607, chance = 30000 }, -- cheese
+	{ name = "worm", chance = 9700, maxCount = 2 },
 }
 
 monster.attacks = {
-	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -8 },
+	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -10 },
 }
 
 monster.defenses = {
 	defense = 5,
 	armor = 1,
-	mitigation = 0.07,
+	mitigation = 0.10,
 }
 
 monster.elements = {
 	{ type = COMBAT_PHYSICALDAMAGE, percent = 0 },
 	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
-	{ type = COMBAT_EARTHDAMAGE, percent = 20 },
-	{ type = COMBAT_FIREDAMAGE, percent = 0 },
+	{ type = COMBAT_EARTHDAMAGE, percent = 0 },
+	{ type = COMBAT_FIREDAMAGE, percent = -10 },
 	{ type = COMBAT_LIFEDRAIN, percent = 0 },
 	{ type = COMBAT_MANADRAIN, percent = 0 },
 	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
-	{ type = COMBAT_ICEDAMAGE, percent = -10 },
-	{ type = COMBAT_HOLYDAMAGE, percent = 20 },
-	{ type = COMBAT_DEATHDAMAGE, percent = -10 },
+	{ type = COMBAT_ICEDAMAGE, percent = 0 },
+	{ type = COMBAT_HOLYDAMAGE, percent = 0 },
+	{ type = COMBAT_DEATHDAMAGE, percent = 0 },
 }
 
 monster.immunities = {
