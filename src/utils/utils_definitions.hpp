8d0bd6f9a3855f5b9dd95dc73ddcbdc3b88b926e
Victor <vasconcellosdevictor@gmail.com>
2025-03-05 19:53:34 -0300

    fix fluids

--- src/utils/utils_definitions.hpp
+++ src/utils/utils_definitions.hpp
@@ -382,6 +382,7 @@ enum Fluids_t : uint8_t {
 	FLUID_CANDY = 19, /* Red with white pieces */
 	FLUID_CHOCOLATE = 20, /* Brown */
 	FLUID_FRESHWATER = 21, /* Blue */
+
 	// 13.40 last fluid is 20, 21+ is a loop from 0 to 20 over and over again
 };
 
