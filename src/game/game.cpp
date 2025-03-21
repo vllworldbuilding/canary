7793dd4286a45ac3438ad95df647103c5a4accf5
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2025-03-21 00:59:43 -0300

    fix: add ui exhausted to imbuement apply/clear (#3455)

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -6067,6 +6067,13 @@ void Game::playerApplyImbuement(uint32_t playerId, uint16_t imbuementid, uint8_t
 		return;
 	}
 
+	if (player->isUIExhausted()) {
+		player->sendCancelMessage(RETURNVALUE_YOUAREEXHAUSTED);
+		return;
+	}
+
+	player->updateUIExhausted();
+
 	if (!player->hasImbuingItem()) {
 		return;
 	}
@@ -6095,6 +6102,13 @@ void Game::playerClearImbuement(uint32_t playerid, uint8_t slot) {
 	if (!player) {
 		return;
 	}
+
+	if (player->isUIExhausted()) {
+		player->sendCancelMessage(RETURNVALUE_YOUAREEXHAUSTED);
+		return;
+	}
+
+	player->updateUIExhausted();
 
 	if (!player->hasImbuingItem()) {
 		return;
