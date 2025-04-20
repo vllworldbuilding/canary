4f03671eeb1f7b49998d10dea3efcb4d704a9262
Victor <vasconcellosdevictor@gmail.com>
2025-04-20 18:38:22 -0300

    update: world board
    update: init

--- data-canary/scripts/actions/objects/world_board.lua
+++ data-canary/scripts/actions/objects/world_board.lua
@@ -1,19 +1,11 @@
 local communicates = {
-	"Welcome to Canary Server! Visit the organization to see all our work: https://github.com/opentibiabr",
-	"Did you like it? Help support what we do here with a donation by visiting the link: https://docs.opentibiabr.com/home/donate/",
-	"Visit our discord to ask questions or report issues: https://discord.gg/gvTj5sh9Mp",
+	"Welcome to The Middlegard Online Shard!"
 }
 
 local scriptConfig = {
 	itemId = 19236,
 	registerPositions = {
-		{ x = 4998, y = 5000, z = 7 },
-		{ x = 4971, y = 5300, z = 5 },
-		{ x = 4802, y = 5088, z = 7 },
-		{ x = 5518, y = 5126, z = 6 },
-		{ x = 5852, y = 5295, z = 5 },
-		{ x = 1940, y = 1345, z = 7 },
-		{ x = 4709, y = 4186, z = 7 },
+		{ x = 20051, y = 19980, z = 7 },
 	},
 }
 
